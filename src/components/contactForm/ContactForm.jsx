2129b38017a23a021de7c23335a332bf5cbba466
jasurumarov <jasurumarov199@gmail.com>
2024-11-03 18:06:38 +0500

    contact finished

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useGetInputValue } from '../../hooks/useGetInputValue'
 
 // Icons
 import Icon1 from '../../assets/icons/socialMedia/youtube.svg'
@@ -10,6 +11,17 @@ import Icon6 from '../../assets/icons/socialMedia/instagram.svg'
 import Icon7 from '../../assets/icons/socialMedia/linkedin.svg'
 import Icon8 from '../../assets/icons/socialMedia/facebook.svg'
 import { Link } from 'react-router-dom'
+
+// Token and ID
+const BOT_TOKEN = "7167621990:AAFEFIaAPwbfqpugbkwxmUFp0CyADqu1J8Y"
+const CHAT_ID = "-1002035440165"
+
+// InitialState
+let initialState = {
+    name: "",
+    number: "",
+    message: ""
+}
 
 const socialData = [
     {
@@ -63,6 +75,25 @@ const socialData = [
 ]
 
 const ContactForm = () => {
+    const { formData, setFormData, handleChange } = useGetInputValue(initialState)
+    const handleSendToTelegram = e => {
+        e.preventDefault()
+        console.log(formData);
+        let text = `FIO: <b>${formData.name}</b>`
+        text += "%0A"
+        text += `Tel: <b>${formData.number}</b>`
+        text += "%0A"
+        text += `Message: <b>${formData.message}</b>`
+        text += "%0A"
+
+        let url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=${text}&parse_mode=html`
+        let api = new XMLHttpRequest()
+        api.open("GET", url, true)
+        api.send()
+
+        setFormData(initialState)
+    }
+
     let socialMedia = socialData?.map(item => (
         <Link key={item.id} to={item.Link} target="_blank" className="rounded-2xl bg-light p-2 text-center dark:bg-dark-2 md:p-4">
             <div className="grid place-content-center rounded-lg bg-white p-6 dark:bg-black">
@@ -92,7 +123,24 @@ const ContactForm = () => {
                     <span className='font-bricolage'>Online</span>
                 </div>
             </div>
-            <div className="mt-10 grid grid-cols-2 gap-6 md:grid-cols-4 lg:mt-14">
+            <form onSubmit={handleSendToTelegram} className='bg-light dark:bg-dark-2 rounded-lg p-6 mt-10 lg:mt-10 grid grid-cols-2 gap-y-6'>
+                <div className='col-span-3 flex flex-col sm:flex-row gap-6'>
+                    <div className='flex flex-col gap-[6px] w-full'>
+                        <label className='text-[15px] font-bricolage dark:text-light/70' htmlFor="name">Ism Familya</label>
+                        <input onChange={handleChange} value={formData.name} required className='w-full py-4 px-6 dark:bg-black font-bricolage rounded-lg border border-solid border-[#DBDFE5] focus:border-dark outline-none dark:border-dark transition-colors' placeholder='Ismingizni kiriting' id='name' name='name' type="text" />
+                    </div>
+                    <div className='flex flex-col gap-[6px] w-full'>
+                        <label className='text-[15px] font-bricolage dark:text-light/70' htmlFor="number">Telefon Raqam</label>
+                        <input onChange={handleChange} value={formData.number} required className='w-full py-4 px-6 dark:bg-black font-bricolage rounded-lg border border-solid border-[#DBDFE5] focus:border-dark outline-none dark:border-dark transition-colors' placeholder='Raqamingizni kiriting' id='number' name='number' type="tel" />
+                    </div>
+                </div>
+                <div className='flex flex-col gap-[6px] col-span-3'>
+                    <label className='text-[15px] font-bricolage dark:text-light/70' htmlFor="message">Xabar</label>
+                    <textarea onChange={handleChange} value={formData.message} required rows={6} className='w-full py-4 px-6 dark:bg-black font-bricolage rounded-lg border border-solid border-[#DBDFE5] focus:border-dark outline-none dark:border-dark transition-colors' placeholder='Xabaringizni kiriting' name="message" id="message"></textarea>
+                </div>
+                <button className='col-span-3 rounded-lg bg-primary py-4 font-bricolage5 text-white '>Jo'natish</button>
+            </form>
+            <div className="mt-10 grid grid-cols-2 gap-6 md:grid-cols-4">
                 {socialMedia}
             </div>
 
